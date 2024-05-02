dfa1a939e382b506f90beedb7753be5eb73db28f
Carlos Augusto <carlosaaugusto2002@outlook.com>
2024-05-02 11:15:22 -0300

    FIX: About Us height

--- src/sections/about-us/AboutUs.tsx
+++ src/sections/about-us/AboutUs.tsx
@@ -32,22 +32,32 @@ export default function AboutUs() {
     }, []);
 
     return (
-        <section className="grid grid-cols-12 grid-rows-12 my-16">
+        <section className="grid grid-cols-12 grid-rows-12 
+        mt-16
+        md:grid-rows-7
+        lg:grid-rows-5 lg:mt-0">
 
             <h1
                 className="col-span-5 col-start-2  font-bold text-2xl  text-sc row-span-1
-                lg:col-start-3 lg:row-start-2
+                lg:col-start-3 lg:row-start-1 lg:flex lg:flex-col lg:justify-center
                 "
             >Sobre nós</h1>
 
-            <div className="col-span-10 row-span-10 col-start-2 row-start-2 text-justify text-sc font-medium text-lg
-            lg:col-span-5 lg:col-start-3 lg:row-start-3 lg:text-left lg:text-sm">
+            <div className="col-span-10 row-span-8 col-start-2 row-start-2 text-justify text-sc font-medium text-lg
+            md:row-span-3
+            lg:col-span-5 lg:col-start-3 lg:row-start-2 lg:text-left lg:text-base">
                 <p>    A Soft Clever tem uma trajetória sólida no desenvolvimento de sistemas desde 1994. Com mais de 12.000 usuários atendidos e inúmeras parcerias estabelecidas com escritórios de contabilidade, nossa empresa está empenhada em oferecer um serviço cada vez mais aprimorado. Nosso segredo reside no compromisso com a excelência, priorizando o bem-estar de nossos clientes e buscando sempre orientá-los para as melhores decisões. Isso resulta em confiança e satisfação em cada interação com nossos serviços.</p>
-                <br />
-                <p>    Contamos com uma equipe altamente qualificada e própria, capacitada para atender às diversas demandas do mercado com agilidade e comprometimento. A fidelidade de nossos clientes é a maior prova da qualidade dos serviços que oferecemos. Desenvolvemos projetos específicos para empresas de todos os tamanhos, desde micro empreendimentos até médias empresas, adaptando-nos às necessidades individuais de cada cliente.</p>
+                {!isMobile && (
+                    <>
+                        <br />
+                        <p>    Contamos com uma equipe altamente qualificada e própria, capacitada para atender às diversas demandas do mercado com agilidade e comprometimento. A fidelidade de nossos clientes é a maior prova da qualidade dos serviços que oferecemos. Desenvolvemos projetos específicos para empresas de todos os tamanhos, desde micro empreendimentos até médias empresas, adaptando-nos às necessidades individuais de cada cliente.</p>
+                    </>
+                )}
             </div>
             {!isMobile && (
-                <div className=" col-span-4 col-start-8 row-start-2 row-span-8 flex flex-col justify-center items-center mr-4 ">
+                <div className=" col-span-4 col-start-8 row-start-1 row-span-8 flex flex-col justify-center items-center mr-4 
+                lg:row-span-5
+                ">
                     <div className=" absolute w-[20%] max-w-[18rem] h-[60%]  max-h-[23.875rem] rounded-xl bg-sc">
                     </div>
                     <Image
@@ -62,9 +72,9 @@ export default function AboutUs() {
             <ButtonExpert
                 color={'blue'}
                 className="gap-2 py-6 px-12 w-full font-semibold  bg-sc text-white hover:bg-scHover 
-            col-span-10 col-start-2 row-start-13 lg:col-start-3 
-            md:mt-2
-            lg:w-[15.25rem]" />
+            col-span-10 col-start-2 row-start-11 lg:col-start-3 
+            md:mt-2 md:row-start-6
+            lg:w-[15.25rem] lg:col-span-3 lg:row-start-5 lg:mt-[2.6rem]" />
 
 
         </section>
