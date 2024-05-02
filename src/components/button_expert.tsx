1cd0bf03d1f1a51cb6e3a1906eb4fcfa35048a03
Carlos Augusto <carlosaaugusto2002@outlook.com>
2024-05-02 13:26:14 -0300

    Update button_expert.tsx

--- src/components/button_expert.tsx
+++ src/components/button_expert.tsx
@@ -11,7 +11,7 @@ interface props {
 }
 export default function ButtonExpert({ className, color = 'white', label = 'Fale com um  especialista' }: props) {
     return (
-        <Button className={`${className}`}  onClick={()=>{window.open('https://api.whatsapp.com/send?phone=5511992810249','_blank')}}>
+        <Button className={`${className}`}  onClick={()=>{window.open('https://web.whatsapp.com/send?phone=5511992810249&text=','_blank')}}>
             <span className={`text-sm md:font-light ${color == 'white' && '!font-semibold'} `}>{label}</span>
             <Image
                 alt="WhatsappIcon"
