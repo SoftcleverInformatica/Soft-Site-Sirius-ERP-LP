aad131caec2b9d6c2aa668ecc9408b68fdbfbb92
Carlos Augusto <carlosaaugusto2002@outlook.com>
2024-05-02 09:12:35 -0300

    UPDT

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { useEffect } from "react";
+import TagManager, { TagManagerArgs } from "react-gtm-module";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,9 +16,27 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  // useEffect(() => {
+  //   const tagManagerArgs: TagManagerArgs = {
+  //     gtmId: 'GTM-N2XFZPPM',
+  //     events: {
+  //       'gtm.start': new Date().getTime(), event: 'gtm.js'
+  //     }
+  //   }
+  //   TagManager.initialize(tagManagerArgs)
+
+  // }, [])
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        {/* <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N2XFZPPM" 
+          height="0" 
+          width="0" 
+          style={{display:"none",visibility:"hidden"}}></iframe>
+        </noscript> */}
+      </body>
     </html>
   );
 }
