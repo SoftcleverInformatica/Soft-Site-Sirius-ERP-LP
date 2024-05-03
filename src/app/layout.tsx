0229fc7b9b20eea3068611892c2e4dafc69bd59a
Carlos Augusto <carlosaaugusto2002@outlook.com>
2024-05-03 12:02:49 -0300

    updt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,26 +16,26 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  // useEffect(() => {
-  //   const tagManagerArgs: TagManagerArgs = {
-  //     gtmId: 'GTM-N2XFZPPM',
-  //     events: {
-  //       'gtm.start': new Date().getTime(), event: 'gtm.js'
-  //     }
-  //   }
-  //   TagManager.initialize(tagManagerArgs)
+  useEffect(() => {
+    const tagManagerArgs: TagManagerArgs = {
+      gtmId: 'GTM-N2XFZPPM',
+      events: {
+        'gtm.start': new Date().getTime(), event: 'gtm.js'
+      }
+    }
+    TagManager.initialize(tagManagerArgs)
 
-  // }, [])
+  }, [])
   return (
     <html lang="en" className="scrollbar-thin scrollbar-track-zinc-100 scrollbar-thumb-scHover scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar-corner-rounded-full">
       <body className={inter.className}>
         {children}
-        {/* <noscript>
+        <noscript>
           <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N2XFZPPM" 
           height="0" 
           width="0" 
           style={{display:"none",visibility:"hidden"}}></iframe>
-        </noscript> */}
+        </noscript>
       </body>
     </html>
   );
