923014791fcaacbe6bcf30bbd9e5dc72ccce224f
Mateus Sales <mateussales98@outlook.com>
2024-05-06 09:29:19 -0300

    change lib for google tag manager for nextjs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,44 +1,28 @@
-"use client"
+import { GoogleTagManager } from '@next/third-parties/google';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { useEffect } from "react";
-import TagManager, { TagManagerArgs } from "react-gtm-module";
-
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Sirius ERP",
-  description: "Soft Clever",
+	title: 'Sirius ERP',
+	description: 'Soft Clever',
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  useEffect(() => {
-    const tagManagerArgs: TagManagerArgs = {
-      gtmId: 'GTM-N2XFZPPM',
-      events: {
-        'gtm.start': new Date().getTime(), event: 'gtm.js'
-      }
-    }
-    TagManager.initialize(tagManagerArgs)
-
-  }, [])
-  return (
-    <html lang="en" className="scrollbar-thin scrollbar-track-zinc-100 scrollbar-thumb-scHover scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar-corner-rounded-full">
-      <body className={inter.className}>
-        {children}
-        <noscript>
-          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N2XFZPPM" 
-          height="0" 
-          width="0" 
-          style={{display:"none",visibility:"hidden"}}></iframe>
-        </noscript>
-      </body>
-    </html>
-  );
+	return (
+		<html
+			lang='pt-br'
+			className='scrollbar-thin scrollbar-track-zinc-100 scrollbar-thumb-scHover scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar-corner-rounded-full'>
+			<body className={inter.className}>
+				<GoogleTagManager gtmId='GTM-N2XFZPPM' />
+				{children}
+			</body>
+		</html>
+	);
 }
