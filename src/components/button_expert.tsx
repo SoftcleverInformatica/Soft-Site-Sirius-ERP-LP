9b25daa28e29a7985a3ab74f7082544b4f29c1a4
Mateus Sales <mateussales98@outlook.com>
2024-06-03 15:42:20 -0300

    feat: Change button component

--- src/components/button_expert.tsx
+++ src/components/button_expert.tsx
@@ -1,28 +1,38 @@
-import { Button } from "./ui/button";
-import WhatsappIcon from "../../assets/whatsapp.svg"
-import WhatsappIconWhite from "../../assets/whatsapp_white.svg"
 import Image from 'next/image';
-
+import WhatsappIcon from '../../assets/whatsapp.svg';
+import WhatsappIconWhite from '../../assets/whatsapp_white.svg';
 
 interface props {
-    className?: string,
-    className2?: string,
-    color?: string,
-    label?: string,
+	className?: string;
+	animation?: string;
+	color?: string;
+	label?: string;
 }
-export default function ButtonExpert({ className,className2, color = 'white', label = 'Fale com um  especialista' }: props) {
-    return (
-        <a 
-        className={className2}
-        href="https://s.tintim.app/whatsapp/039a35a9-d169-4b36-98c3-cfef6a71d234/21710a1b-daa7-48be-a44d-2f66d3526f1f" target="_blank">
-
-            <Button className={`${className}`}  >
-                <span className={`text-sm md:font-light ${color == 'white' && '!font-semibold'} `}>{label}</span>
-                <Image
-                    alt="WhatsappIcon"
-                    src={color == 'white' ? WhatsappIcon : WhatsappIconWhite}
-                />
-            </Button>
-        </a>
-    )
+export default function ButtonExpert({ className, animation, color = 'white', label = 'Fale com um  especialista' }: props) {
+	return (
+		<a
+			className={`
+				flex items-center justify-center gap-2 py-2 px-12 rounded-md  hover:opacity-90
+				${color === 'white' ? 'bg-white  text-sc hover:bg-slate-500 ' : ''}
+				${color === 'orange' ? 'bg-orange-700 hover:bg-orange-600' : ''}
+				${color === 'blue' ? 'bg-sc' : ''} 
+				${animation ? animation : ''} 
+				${className}
+			`}
+			href='https://s.tintim.app/whatsapp/039a35a9-d169-4b36-98c3-cfef6a71d234/21710a1b-daa7-48be-a44d-2f66d3526f1f'
+			target='_blank'>
+			<span
+				className={`
+					text-sm  
+					${color == 'white' && 'font-semibold'} 
+					${color == 'white' ? 'text-sc' : 'text-white'}
+				`}>
+				{label}
+			</span>
+			<Image
+				alt='WhatsappIcon'
+				src={color == 'white' ? WhatsappIcon : WhatsappIconWhite}
+			/>
+		</a>
+	);
 }
