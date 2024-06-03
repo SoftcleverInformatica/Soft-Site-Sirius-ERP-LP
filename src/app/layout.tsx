9d86b6d78ad6a6fd91fb182551858256e3cc5d7f
Mateus Sales <mateussales98@outlook.com>
2024-06-03 15:35:00 -0300

    feat: change font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import { GoogleTagManager } from '@next/third-parties/google';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Montserrat } from 'next/font/google';
 import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
+const font = Montserrat({ subsets: ['latin'] });
+
 
 export const metadata: Metadata = {
 	title: 'Sirius ERP',
@@ -19,7 +20,7 @@ export default function RootLayout({
 		<html
 			lang='pt-br'
 			className='scrollbar-thin scrollbar-track-zinc-100 scrollbar-thumb-scHover scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar-corner-rounded-full'>
-			<body className={inter.className}>
+			<body className={font.className}>
 				<GoogleTagManager gtmId='GTM-N2XFZPPM' />
 				{children}
 			</body>
