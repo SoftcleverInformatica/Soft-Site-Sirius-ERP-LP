c1967751c60fae744b2bd0d6eadbbfd909cedccd
mateussales98 <mateussales98@outlook.com>
2024-08-26 15:10:05 -0300

    feat: accordion done

--- src/sections/funcionalidades/Funcionalidades.tsx
+++ src/sections/funcionalidades/Funcionalidades.tsx
@@ -1,27 +1,31 @@
+'use client';
+
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion';
 import { ArrowRight } from '@phosphor-icons/react/dist/ssr';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
 export default function Funcionalidades() {
 	const recursos = [
 		{
-			key: 'Vendas',
-			value: [
+			modulo: 'Vendas',
+			funcionalidades: [
+				{ titulo: '(PDV)', descricao: 'Contas a pagar e a receber', imagem: 'Screenshot do módulo de gestão financeira.' },
+				{ titulo: 'Hub de Integração', descricao: 'Estoque em tempo real', imagem: 'Screenshot do módulo de gestão de estoque.' },
+				{ titulo: 'Vendas Realizadas', descricao: 'Do pedido até a entrega.', imagem: 'Screenshot do módulo de gestão de vendas.' },
+			],
+		},
+		{
+			modulo: 'Fiscal',
+			funcionalidades: [
 				{ titulo: 'Ponto de Venda (PDV)', descricao: 'Contas a pagar e a receber', imagem: 'Screenshot do módulo de gestão financeira.' },
 				{ titulo: 'Hub de Integração', descricao: 'Estoque em tempo real', imagem: 'Screenshot do módulo de gestão de estoque.' },
 				{ titulo: 'Vendas Realizadas', descricao: 'Do pedido até a entrega.', imagem: 'Screenshot do módulo de gestão de vendas.' },
 			],
 		},
 		{
-			key: 'Fiscal',
-			value: [
-				{ titulo: 'Ponto de Venda (PDV)', descricao: 'Contas a pagar e a receber', imagem: 'Screenshot do módulo de gestão financeira.' },
-				{ titulo: 'Hub de Integração', descricao: 'Estoque em tempo real', imagem: 'Screenshot do módulo de gestão de estoque.' },
-				{ titulo: 'Vendas Realizadas', descricao: 'Do pedido até a entrega.', imagem: 'Screenshot do módulo de gestão de vendas.' },
-			],
-		},
-		{
-			key: 'Confecção',
-			value: [
+			modulo: 'Confecção',
+			funcionalidades: [
 				{ titulo: 'Ponto de Venda (PDV)', descricao: 'Contas a pagar e a receber', imagem: 'Screenshot do módulo de gestão financeira.' },
 				{ titulo: 'Hub de Integração', descricao: 'Estoque em tempo real', imagem: 'Screenshot do módulo de gestão de estoque.' },
 				{ titulo: 'Vendas Realizadas', descricao: 'Do pedido até a entrega.', imagem: 'Screenshot do módulo de gestão de vendas.' },
@@ -29,29 +33,22 @@ export default function Funcionalidades() {
 		},
 	];
 
-	const exibirItemMenu = recursos.map((_, i) => (
-		<AccordionItem key={i} value={`item-${i}`} className='border-0 '>
-			<AccordionTrigger className='text-base font-bold border-2  rounded-xl p-1 px-3'>
-				<span className=''>{_.key}</span>
-			</AccordionTrigger>
-			{_.value.map((_, j) => (
-				<AccordionContent key={j} className=' px-6 py-2 text-base font-semibold '>
-					<a href='#' className='flex justify-between items-center'>
-						<span>{_.descricao}</span> <ArrowRight></ArrowRight>
-					</a>
-				</AccordionContent>
-			))}
-		</AccordionItem>
-	));
-	const exibirDetalhes = recursos.map((_, i) => (
-		<>
-			{_.value.map((_, j) => (
-				<a href='#' className='flex justify-between items-center'>
-					<span>{_.descricao}</span> <ArrowRight></ArrowRight>
-				</a>
-			))}
-		</>
-	));
+	const [selecionado, setSelecionado] = useState<{ modulo: string; item: number }>({ modulo: recursos[0].modulo, item: 0 });
+
+	const item = (modulo: string, item: number) => {
+		let exibirDetalhes = recursos.filter((item: any) => item.modulo === modulo);
+
+		return (
+			<div key={Math.random()} className=' p-6 opacity-0 animate-fadeIn '>
+				<h3>{exibirDetalhes[0].funcionalidades[item].titulo}</h3>
+				<h6>{exibirDetalhes[0].funcionalidades[item].descricao}</h6>
+				<Image alt='' src={'#'} width={1000} height={1000} className='h-10 w-10' />
+				{/* <Image alt='' src={exibirDetalhes[0].funcionalidades[item].imagem} width={1000} height={1000} className='h-10 w-10' /> */}
+			</div>
+		);
+	};
+
+	useEffect(() => {}, []);
 
 	return (
 		<section className='w-full flex flex-col items-center ' id='funcionalidades'>
@@ -59,11 +56,24 @@ export default function Funcionalidades() {
 				<h2 className='text-3xl lg:text-5xl text-center font-extrabold leading-tight tracking-wide font-main'>Suas necessidades em um só lugar</h2>
 				<div className='flex flex-col lg:flex-row justify-between gap-10 border-2 rounded-[40px] '>
 					<div className='w-1/2 '>
-						<Accordion className='flex flex-col gap-4 p-6 bg-white h-full w-full rounded-l-[40px]' type='single' collapsible>
-							{exibirItemMenu}
+						<Accordion className='flex flex-col gap-4 p-6 bg-white h-full w-full rounded-l-[40px]' type='single' collapsible defaultValue='item-0'>
+							{recursos.map((recurso, i) => (
+								<AccordionItem key={i} value={`item-${i}`} className='border-0'>
+									<AccordionTrigger className='text-base font-bold border-2 hover:no-underline rounded-xl p-1 px-3 ' onClick={() => setSelecionado({ modulo: recurso.modulo, item: 0 })}>
+										<span className={`text-base font-bold ${recurso.modulo === selecionado.modulo ? 'text-primary' : ''}`}>{recurso.modulo}</span>
+									</AccordionTrigger>
+									{recurso.funcionalidades.map((_2, j) => (
+										<AccordionContent key={j} className={`px-6 py-2 text-base font-semibold  ${selecionado.item === j ? 'text-primary' : ''} `}>
+											<a onClick={() => setSelecionado({ modulo: recurso.modulo, item: j })} className='flex justify-between items-center'>
+												<span>{_2.titulo}</span> <ArrowRight></ArrowRight>
+											</a>
+										</AccordionContent>
+									))}
+								</AccordionItem>
+							))}
 						</Accordion>
 					</div>
-					<div className='w-1/2 bg-primary rounded-r-[40px] text-white '>{exibirDetalhes}</div>
+					<div className='w-1/2 bg-primary rounded-r-[40px] text-white '>{item(selecionado.modulo, selecionado.item)}</div>
 				</div>
 			</div>
 		</section>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -74,11 +74,16 @@ const config = {
           "0%": { transform: 'translateX(-100%)' },
           "100%": { transform: "translateX(100%)" },
         },
+        'fade-in': {
+          '0%': { opacity: "0" },
+          '100%': { opacity: "1" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
         "range": 'fade-in-left-out-right 6s linear infinite ',
+        fadeIn: 'fade-in 0.5s ease-in-out 0.25s 1 forwards',
       },
       backgroundImage: {
         'base-lp': "url('/assets/base.png')",
